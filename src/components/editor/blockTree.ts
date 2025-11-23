1079aa4f57f55f921212decc2234fe4b75512d51
Atharva <atharvasapre@gmail.com>
2025-11-23 15:05:21 -0500

    Set notify-until interval default to 0.2s

--- src/components/editor/blockTree.ts
+++ src/components/editor/blockTree.ts
@@ -97,7 +97,7 @@ export const createDefaultBlock = (type: BlockType): Block => {
         label: "Beep",
         timeoutMs: 10000,
         soundType: "default",
-        interval: 0.3
+        interval: 0.2
       } as Block;
     case "loop":
     default:
